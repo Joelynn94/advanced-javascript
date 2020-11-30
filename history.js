a4b220553613f28dfa5cd15242f656fe51d1cda9
Joe Lynn <josephlynn94@gmail.com>
2020-11-30 07:40:14 -0500

    history, compilers. interpreters, javascript engines

---
+++ history.js
@@ -0,0 +1,9 @@
+/*
+  By default, computers do not know or understand JavaScript. There needs to be an engine in between the JavaScript code and the computer. 
+
+  JavaScript was created by Brendan Eich in 1995. Also built the first JavaScript engine SpiderMonkey. 
+
+  V8 JavaScript engine was created in 2008 by Google (the project creator was Lars Bak).
+  
+*/
+
---
+++ interpreters-compilers.js
@@ -0,0 +1,12 @@
+/*
+  Interpreters run through a program line by line and translates the program into machine code. Interpreted languages were once significantly slower than compiled languages. But, with the development of just-in-time compilation, that gap is shrinking.
+  -PHP, Ruby, Python, and JavaScript.
+
+  Compiled languages are converted directly into machine code that the processor can execute. As a result, they tend to be faster and more efficient to execute than interpreted languages. They also give the developer more control over hardware aspects, like memory management and CPU usage.
+  -C, C++, Erlang, Haskell, Rust, and Go.
+
+  Babel is a Javascript compiler that takes your modern JS code and returns  browser compatible JS (older JS code).
+  Typescript is a superset of Javascript that compiles down to Javascript.
+
+  Both of these do exactly what compilers do: Take one language and convert into a different one!
+*/
